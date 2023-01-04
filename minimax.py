68d26580b9b6e95e8ec2c26dd818691ea06f43f3
ShervinEmrani <emranishervin@gmail.com>
2023-01-04 13:17:14 +0330

    minimax function added

--- minimax.py
+++ minimax.py
@@ -1,3 +1,40 @@
+import copy
+
+
+def minimax(board, depth, maximizer, active_player):
+    if depth == 0 or board.end_game():
+        return None, evaluate(board, active_player)
+
+    children = board.successor(board)
+    # best_move = children[0]
+
+    if maximizer:
+        max_list = []
+        for child in children:
+            board_copy = copy.deepcopy(child)
+            current_eval = minimax(board_copy, depth - 1, False, active_player)[1]
+            tup = (child, current_eval)
+            max_list.append(tup)
+
+        max_tuple = max(max_list, key=lambda p: p[1])
+        best_move = max_tuple[0]
+        max_value = max_tuple[1]
+        return best_move, max_value
+
+    else:
+        min_list = []
+        for child in children:
+            board_copy = copy.deepcopy(child)
+            current_eval = minimax(board_copy, depth - 1, True, active_player)[1]
+            tup = (child, current_eval)
+            min_list.append(tup)
+
+        min_tuple = min(min_list, key=lambda p: p[1])
+        best_move = min_tuple[0]
+        min_value = min_tuple[1]
+        return best_move, min_value
+
+# w = max(players, key=lambda p: p.totalScore
 
 
 def evaluate(board, player):
